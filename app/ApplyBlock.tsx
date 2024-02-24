619f932376bc7c6e88b3436c36bd58752f3e4384
Wilbur Hu <wilburhu83@gmail.com>
2024-02-24 13:35:22 +0800

    Update

--- app/ApplyBlock.tsx
+++ app/ApplyBlock.tsx
@@ -28,7 +28,7 @@ export default function ApplyBlock(props: ApplyBlockProps) {
           Be the first to test
         </h2>
         <p className="applyBlock__descriptionContent text-white text-center  md:text-left pt-4">
-          Hi, I'm Louis Graham, the founder of the company. Book a demo call
+          Hi, I`&apos;`m Louis Graham, the founder of the company. Book a demo call
           with me to become a beta tester for our app and kickstart your
           company. Apply for access below and I’ll be in touch to schedule a
           call.
